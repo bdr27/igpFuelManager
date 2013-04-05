5d439d52f3244c39d9cf9ca67f76614769232722
Brendan Russo <brendanrusso27@gmail.com>
2013-04-05 11:45:37 +1000

    put something in each package

---
+++ src/au/bdr27/igpFuelManager/GUI/TestGUI.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package au.bdr27.igpFuelManager.GUI;
+
+/**
+ *
+ * @author Brendan
+ */
+public class TestGUI {
+    
+}
